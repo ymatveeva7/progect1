0d589ce5cd3ce3f95c5124dc335b4fdf7e6579f4
ymatveeva7 <yana.matveeva.99@list.ru>
2017-11-05 13:00:08 +0400

    jsok

---
+++ js/js.js
@@ -0,0 +1,17 @@
+function buttone(){
+    var darkLayer = document.createElement('div'); // слой затемнения
+    darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+    document.body.appendChild(darkLayer); // включаем затемнение
+    var modalWin = document.getElementById('popupWin'); // находим наше "окно"
+    modalWin.style.display = 'block'; // "включаем" его
+    darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
+        darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+        modalWin.style.display = 'none'; // делаем окно невидимым
+        return false;};
+}
+function hov(){
+    var darkLayer = document.createElement('div'); // слой затемнения
+    darkLayer.id = 'color'; // id чтобы подхватить стиль
+    document.body.appendChild(darkLayer); // включаем затемнение
+
+};
