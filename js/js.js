db6081b834825077d664787f151de62674e07baa
ymatveeva7 <yana.matveeva.99@list.ru>
2017-11-11 20:20:09 +0400

    jsss

--- js/js.js
+++ js/js.js
@@ -10,36 +10,3 @@ function buttone(){
                     modalWin.style.display = 'none'; // делаем окно невидимым
 				 return false;};
 };
-
-//function kk(){
-//var hov1=document.querySelector('#l1');
-//var hov2=document.querySelector('#l2');
-//var hov3=document.querySelector('#l3');
-//var divka=document.querySelectorAll('.cirle');
-//hov1.addEventListener('click',function{
-//cirle[0].style.background='white';
-//cirle[1].style.background='none';
-//cirle[2].style.background='none';
-//});
-//hov2.addEventListener('click',function{
-//cirle[0].style.background='none';
-//cirle[1].style.background='white';
-//cirle[2].style.background='none';
-//});
-//hov3.addEventListener('click',function{
-//cirle[0].style.background='none';
-//cirle[1].style.background='none';
-//cirle[2].style.background='white';
-//});
-//};
-//function podpiska(){
-  //              var okno = document.createElement('div'); // слой затемнения
-    //            okno.id = 'window'; // id чтобы подхватить стиль
-      //          document.body.appendChild(okno); // включаем затемнение
-		//		 var frame = document.getElementById('win'); // находим наше "окно"
-          //      frame.style.display = 'block'; // "включаем" его
-			//	okno.onclick = function () {  // при клике на слой затемнения все исчезнет
-              //      okno.parentNode.removeChild(okno); // удаляем затемнение
-                //    frame.style.display = 'none'; // делаем окно невидимым
-				 //return false;};
-//};
